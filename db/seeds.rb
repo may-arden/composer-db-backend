da679d6d65d09ece85ef1fa92bb621cd44d57175
may arden <mayard3n@gmail.com>
2021-10-20 01:31:07 -0700

    runs migration and seeds db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: "baroque")
+Category.create(name: "classical")
+Category.create(name: "romantic")
+Category.create(name: "modern")
+
